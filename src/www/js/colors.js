6aded6effb6dcf37dab03b4dc16c425871ee4e4d
Tarsis <br.tmvdl@gmail.com>
2023-06-04 15:24:48 -0300

    Updating Colors file

--- src/www/js/colors.js
+++ src/www/js/colors.js
@@ -2,7 +2,9 @@
 const COLORS = {
   WHITE: 'rgb(255, 255, 255)',
   YELLOW: 'rgb(255, 255, 0)',
+  RED: 'rgb(255, 0, 0)',
   BLACK: 'rgb(0, 0, 0)',
+  GRAY: 'rgb(153, 153, 153)',
 }
 
 export default COLORS
